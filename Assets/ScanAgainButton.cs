f28e5e1a1592125de72e994984ed2e3247d447af
AmirhH <amirh18@gmail.com>
2016-10-08 01:00:11 +0300

    First
    first

---
+++ Assets/ScanAgainButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ScanAgainButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ScanAgain()
+    {
+        SceneManager.LoadScene("Vuforia-3-UserDefinedTargets", LoadSceneMode.Additive);
+        Application.LoadLevel(0);
+    }
+
+    
+}
