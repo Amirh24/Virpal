566f2b309fb7b9941d57225e584331fc44f35455
AmirhH <amirh18@gmail.com>
2016-10-13 01:30:21 +0300

    Working on tutorial image

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+
+   // private TutorialImage mQualityDialog;
+
+    void Start () {
+	
+	}
+	
+    void RemoveTutorialImage()
+    {
+
+    }
+}
