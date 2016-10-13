3bdf712b73ac58ab61c656ca8a68611989b29d85
AmirhH <amirh18@gmail.com>
2016-10-13 03:38:38 +0300

    Finished Functionallityo of tutorial screenIn
    Including "Dont show this again" checkbox

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -5,17 +5,25 @@ using UnityEngine.UI;
 
 public class TutorialScript : MonoBehaviour {
 
-    // private TutorialImage mQualityDialog;
     private RawImage mTutorialRawImage;
     private Button mTutorialButton;
     private Toggle mTutorialToggle;
 
+    public int testingVersion = 2; // Because player prefs is kept in memory, we will change thsi number as long as we test
+
 
     void Start () {
+        print("Testing version number is " + testingVersion + " and version in memory is " + PlayerPrefs.GetInt("DontShowTutorial"));
+
         mTutorialRawImage = GetComponentInChildren<RawImage>();
         mTutorialButton = GetComponentInChildren<Button>();
         mTutorialToggle = GetComponentInChildren<Toggle>();
 
+        if ((PlayerPrefs.GetInt("DontShowTutorial") == testingVersion)) {
+            mTutorialRawImage.gameObject.SetActive(false);
+            mTutorialButton.gameObject.SetActive(false);
+            mTutorialToggle.gameObject.SetActive(false);
+        }
     }
 
     public void RemoveTutorial()
@@ -23,5 +31,13 @@ public class TutorialScript : MonoBehaviour {
         mTutorialRawImage.gameObject.SetActive(false);
         mTutorialButton.gameObject.SetActive(false);
         mTutorialToggle.gameObject.SetActive(false);
+    }
+
+    public void DontShowTutorialAgain()
+    {
+        if ((PlayerPrefs.GetInt("DontShowTutorial") != testingVersion))
+            {
+            PlayerPrefs.SetInt("DontShowTutorial", testingVersion);
+            }
     }
 }
