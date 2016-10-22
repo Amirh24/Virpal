81dd12470fa62a8584a1eb054dfe0fc0c7846c8b
AmirhH <amirh18@gmail.com>
2016-10-22 04:44:47 +0300

    Created an exam[ple Main menu incuding script

---
+++ Assets/menuManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CheckGameStatus();
+	}
+	
+
+    void CheckGameStatus() {
+
+        if(PlayerPrefs.GetInt("GameAlreadyStarted") == 0) // A game never started
+        {
+            // disable continue button
+        }
+    }
+
+   public void StartNewGame() {
+        if(PlayerPrefs.GetInt("GameAlreadyStarted") == 1)
+        {
+            // Alert : Are you sure ? Previous pets will be erased
+        }
+
+        PlayerPrefs.SetInt("GameAlreadyStarted", 1);
+        //Load New Game Scene
+    }
+
+    public void ContinueGame() {
+
+        //Load Main Game Scene
+    }
+
+}
